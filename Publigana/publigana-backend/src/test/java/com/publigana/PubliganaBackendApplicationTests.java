6d9236cc1e6e14dc386eee2614f191a18151adf0
BGutierrezFlorez <bratciney@gmail.com>
2026-08-05 18:08:46 -0500

    Add SonarQube configuration, enhance error logging, and refactor role handling in AuthService

--- Publigana/publigana-backend/src/test/java/com/publigana/PubliganaBackendApplicationTests.java
+++ Publigana/publigana-backend/src/test/java/com/publigana/PubliganaBackendApplicationTests.java
@@ -3,7 +3,9 @@ package com.publigana;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.profiles.active=test"
+})
 class PubliganaBackendApplicationTests {
 
     @Test
