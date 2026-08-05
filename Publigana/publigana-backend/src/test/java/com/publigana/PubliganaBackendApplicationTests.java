1c37c0825abafbaa8a2b57c57c886008871cdf5d
BGutierrezFlorez <bratciney@gmail.com>
2026-08-04 19:38:11 -0500

    Update pom.xml dependencies, add Flyway baseline configuration, and create test profile properties

--- Publigana/publigana-backend/src/test/java/com/publigana/PubliganaBackendApplicationTests.java
+++ Publigana/publigana-backend/src/test/java/com/publigana/PubliganaBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.publigana;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PubliganaBackendApplicationTests {
 
     @Test
